7e042b38c4e286dbbeb34ccbafb94a803c3263b3
JoyOfSRC <joyofsrc@gmail.com>
2014-08-26 11:00:59 +0100

    directory reshuffled

--- layouts/3.php
+++ layouts/3.php
@@ -6,7 +6,10 @@
 		<title>Testing the content</title>
 		<link rel="stylesheet" href="<?=$src[0]["cfg"]->o["static"];?>/src/cwv/<?=$src[0]["var"]->mtf(array("typ"=>"css","dta"=>"cwv"));?>.css">
 		<!---<script src="<?=$src[0]["cfg"]->o["static"];?>/src/cke/ckeditor.js"></script>-->
-	</head>	
+	</head>
+	
+	<!-- TODO: WHEN LOADING IMAGES IT IS NECESSARY TO USE A TRY_FILES IN THE NGINX CONFIGURATION TO DEAL WITH MISSING IC's -->
+	
 	<body>
 		
 		<div id="text-holder" contenteditable="true">
@@ -15,7 +18,7 @@
 		<div class="row">
 			<div class="col all25 hgt20" style="background-color:#f00;"></div>
 			<div class="col all25 hgt20" style="background-color:#0f0;"></div>
-			<img width="100" height="100" src="<?=$src[0]["cfg"]->o["static"];?>/src/fiwv/100x100/100.jpg">
+			<img width="450" height="481" src="<?=$src[0]["cfg"]->o["static"];?>/src/fiwv/100x100/100.jpg">
 		</div>
 		<script>
 			/* CKEDITOR.inline(document.getElementById( 'text-holder' )); */
